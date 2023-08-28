193d9062dd0616fed50f3a40c1618d1015504aea
yasin ben omar <123163314+Y4s1n1980@users.noreply.github.com>
2023-08-28 23:36:37 +0200

    Delete src directory

--- src/setupTests.js
+++
@@ -1,6 +0,0 @@
-//"jest-dom" agrega matchers personalizados de jest para hacer afirmaciones sobre nodos del DOM. 
-//Esto te permite hacer cosas como: 
-//"expect(elemento).toHaveTextContent(/react/i)". 
-//Si deseas obtener más información, 
-//puedes aprender más en: https://github.com/testing-library/jest-dom.
-import '@testing-library/jest-dom';
